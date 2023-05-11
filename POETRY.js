3f596286f9b084e5743ea561ec073cef970a89c2
Vedant Borkar <vedantborkar1234@gmail.com>
2023-05-12 00:19:44 +0530

    Update POETRY.js

--- POETRY.js
+++ POETRY.js
@@ -1,7 +1,7 @@
 const fetchPoemButton = document.getElementById('fetch-poem-button');
 const poemDiv = document.getElementById('poem');
 
-document.addEventListener('DOMContentLoaded', function () {
+function fetchAndDisplayPoem() {
     fetch('https://poetrydb.org/random')
         .then(response => response.json())
         .then(function (poem) {
@@ -26,31 +26,8 @@ document.addEventListener('DOMContentLoaded', function () {
             poemDiv.appendChild(authorElement);
             poemDiv.appendChild(linesElement);
         });
-});
+}
 
-fetchPoemButton.addEventListener('click', function () {
-    fetch('https://poetrydb.org/random')
-        .then(response => response.json())
-        .then(function (poem) {
-            const title = poem[0]['title'];
-            const author = poem[0]['author'];
-            const lines = poem[0]['lines'];
-            const titleElement = document.createElement('h2');
-            titleElement.textContent = title;
-            titleElement.style.cssText = 'padding-top: 3rem;color: wheat;';
-            const authorElement = document.createElement('h5');
-            authorElement.textContent = `By ${author}`;
-            authorElement.style.cssText = 'padding-top: 1rem;padding-bottom: 3rem;'
-            const linesElement = document.createElement('div');
-            linesElement.style.cssText = 'padding-bottom: 3rem;';
-            lines.forEach(function (line) {
-                const lineElement = document.createElement('p');
-                lineElement.textContent = line;
-                linesElement.appendChild(lineElement);
-            });
-            poemDiv.innerHTML = '';
-            poemDiv.appendChild(titleElement);
-            poemDiv.appendChild(authorElement);
-            poemDiv.appendChild(linesElement);
-        });
-});
+document.addEventListener('DOMContentLoaded', fetchAndDisplayPoem);
+
+fetchPoemButton.addEventListener('click', fetchAndDisplayPoem);
